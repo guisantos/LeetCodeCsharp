77e6472f33171978600f83c9c8f6cb73e8b0756d
Guilherme Flores <guilhermeflores@obi.io>
2025-06-14 14:34:38 +0100

    add solution to 9

---
+++ 20-ValidParenthesis.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode;
+public class ValidParenthesis
+{
+    // https://www.youtube.com/watch?v=YwvHeouhy6s
+    // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+    // determine if the input string is valid.
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        var pairs = new Dictionary<char, char>
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+
+        foreach (var bracket in s)
+        {
+            // if dictionary contains the opening bracket, push it onto the stack
+            if (pairs.ContainsKey(bracket))
+            {
+                stack.Push(bracket);
+            }
+            // if the stack is empty or the current closing bracket does not match the last opening bracket
+            else if (stack.Count == 0 || bracket != pairs[stack.Pop()])
+            {
+                return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+
+
+    // Alternative implementation using an array as a stack
+    public bool IsValidArray(string s)
+    {
+        // Early exit if string length is odd (can't be valid)
+        if (s.Length % 2 != 0)
+            return false;
+
+        char[] stack = new char[s.Length];
+        int top = -1;
+
+        foreach (char c in s)
+        {
+            // Push if opening
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack[++top] = c;
+            }
+            else
+            {
+                // If stack is empty or top doesn't match opening pair
+                if (top < 0 ||
+                    (c == ')' && stack[top] != '(') ||
+                    (c == '}' && stack[top] != '{') ||
+                    (c == ']' && stack[top] != '['))
+                {
+                    return false;
+                }
+                top--; // Pop the stack
+            }
+        }
+
+        // Valid if stack is empty
+        return top == -1;
+    }
+}
---
+++ 9-PalindromeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode;
+public class PalindromeNumber
+{
+    //https://www.youtube.com/watch?v=OlTk8wM48ww
+    // palindrome number is a number that reads the same backward as forward
+    // e.g. 121, 12321,
+    // 0, -121 is not a palindrome because it reads as 121-
+    // // 10, 100, 1000 is not a palindrome because it reads as 01, 001, 0001
+    public bool IsPalindrome(int x)
+    {
+        // negative number is never palindrome
+        // a number finished with 0 is not a palindrome
+        if (x < 0 || (x != 0 && x % 10 == 0))
+        {
+            return false;
+        }
+
+        int half = 0;
+        // e.g 1221 
+        while (x > half)
+        {
+            // half = (0 * 10 = 0) + (1221 % 10 = 1)
+            // half = 0 + 1 = 1
+            // half = (1 * 10 = 10) + (122 % 10 = 2)
+            // half = 10 + 2 = 12
+            half = (half * 10) + (x % 10);
+            // x = 1221 / 10 = 122
+            // we basically moved the 1 from 1221 to the other half
+            // x = 122 / 10 = 12
+            x /= 10;
+        }
+
+        // if the length of the number is odd, we can get rid of the middle digit by half / 10
+        return x == half || x == half / 10;
+    }
+}
